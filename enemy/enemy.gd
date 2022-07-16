d0a07628e46f879446b75d5f89b90845def961c1
Alex Darigan <47497501+AlexDarigan@users.noreply.github.com>
2022-07-16 12:28:16 +0100

    Begin Battle

--- enemy/enemy.gd
+++ enemy/enemy.gd
@@ -1,34 +1,29 @@
 extends CharacterBody2D
+class_name Enemy
 
-@export var face_change_timer: Timer
+@export var timer: Timer
 @export var sprite: Sprite2D
 @export var speed: float = 300.0
 @export var jump: float = -400.0
 @export var rotation_speed: float = 6
 @export var faces: Array[Texture] = []
 @export var limit: int = 0
-var _current_face: int
+var _current_face: int = 0
 # Get the gravity from the project settings to be synced with RigidDynamicBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var direction: int = 0
+var direction: int = 1
+
+
 
 func _physics_process(delta: float) -> void:
+	velocity.x = direction * speed
 	if not is_on_floor():
 		velocity.y += gravity * delta
-		sprite.rotation += rotation_speed * delta * direction
-
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = jump
-
-	direction = Input.get_axis("move_left", "move_right")
-	if direction:
-		velocity.x = direction * speed
-		sprite.rotation += rotation_speed * delta * direction
-	else:
-		sprite.rotation = 0
-		velocity.x = move_toward(velocity.x, 0, speed)
-
 	move_and_slide()
+	sprite.rotation += rotation_speed * delta * direction
+	
+	if is_on_wall():
+		direction = -direction
 
 func _change_face() -> void:
 	_current_face = randi() % (faces.size() - limit)
@@ -36,11 +31,3 @@ func _change_face() -> void:
 
 func _on_face_change_timer_timeout():
 	_change_face()
-
-
-func _on_battle_zone_body_entered(body):
-	if body is Enemy:
-		body.direction = 0
-		body.timer.stop()
-		face_change_timer.stop()
-		print("do fight")
